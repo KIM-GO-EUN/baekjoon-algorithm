be5eb7bd40623e9bdc12218b6a7192c6cf8130e5
KIM-GO-EUN <68186240+KIM-GO-EUN@users.noreply.github.com>
2023-01-02 22:49:16 +0900

    [Bronze III] Title: 곱셈, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        int one = B % 10;
+        int ten = ((B/10)%10);
+        int onehundred = B/100;
+        
+        System.out.println(A*one);
+        System.out.println(A*ten);
+        System.out.println(A*onehundred);
+        System.out.println(A*B);
+    }
+}
