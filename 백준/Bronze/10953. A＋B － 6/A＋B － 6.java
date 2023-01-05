bbc51dc8258a7337aa1fbcfee0f39a30e061c07b
KIM-GO-EUN <68186240+KIM-GO-EUN@users.noreply.github.com>
2023-01-05 22:23:26 +0900

    [Bronze II] Title: A+B - 6, Time: 120 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int t = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<t; i++){
+            st = new StringTokenizer(br.readLine(), ",");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b);
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
