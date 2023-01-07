65bb1aec9b04a850d32399a10af834ac1d8202b3
KIM-GO-EUN <68186240+KIM-GO-EUN@users.noreply.github.com>
2023-01-07 11:58:23 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.java
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.java
@@ -10,7 +10,7 @@ public class Main{
          for(int i=0; i<str.length(); i++){
              System.out.print(str.charAt(i));
              if((i+1)%10==0){
-                 System.out.println("");
+                 System.out.println();
              }
          }
      } 
