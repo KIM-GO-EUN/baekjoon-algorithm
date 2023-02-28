088b706db379ccb0007f1e042441e01f9e418924
goeun <68186240+KIM-GO-EUN@users.noreply.github.com>
2023-02-28 14:34:44 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,8 +13,7 @@ class Main{
             sb.append(1);
         }else{
             sb.append(0);
-        }
-        
+        }     
         System.out.print(sb);
     }
 }
