46d95d8164d90ab33bcccf958ba28ccd32af8464
goeun <68186240+KIM-GO-EUN@users.noreply.github.com>
2023-03-02 12:59:47 +0900

    [Bronze V] Title: 사파리월드, Time: 120 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/2420. 사파리월드/사파리월드.java
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -6,13 +6,10 @@ import java.util.StringTokenizer;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");        
         long n = Long.parseLong(st.nextToken());
         long m = Long.parseLong(st.nextToken());
         
-		System.out.println(Math.abs(n-m));
-        
+		System.out.println(Math.abs(n-m)); 
     }
 }
